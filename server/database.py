02db2adde0b23a71fedb4f5891ae0dfe3714f047
RashmitVartak <rashmitvartak@gmail.com>
2026-05-17 20:48:24 +0530

    optimized the database code

--- server/database.py
+++ server/database.py
@@ -3,7 +3,7 @@ import os
 
 DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "goal_portal.db")
 
-
+##
 def get_db():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
